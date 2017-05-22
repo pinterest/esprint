d8deded7ab2dab29ec3c8210c558d758945187d3
Allen Kleiner <allenk@pinterest.com>
2017-05-22 14:13:25 -0700

    Remove unnecessary newline

--- src/commands/kill.js
+++ src/commands/kill.js
@@ -1,7 +1,6 @@
 import path from 'path';
 import { execSync } from 'child_process';
 
-
 export const killPort = () => {
   const command = `lsof -i TCP:5004 | grep LISTEN | awk '{print $2}' | xargs kill -9`;
   execSync(command);
