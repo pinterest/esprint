6e97fc50f7ac617847b074c6df38c08b31eb0168
Allen Kleiner <allenk@pinterest.com>
2017-05-22 17:24:49 -0700

    Add esprint run command

---
+++ src/commands/run.js
@@ -0,0 +1,64 @@
+import LintRunner from '../LintRunner';
+import glob from 'glob';
+import path from 'path';
+import fs from 'fs';
+import { CLIEngine } from 'eslint';
+
+const ROOT_DIR = process.cwd();
+const eslint = new CLIEngine({ cwd: ROOT_DIR });
+
+const processEsprintrc = (rcPath) => {
+  const rc = JSON.parse(fs.readFileSync(rcPath));
+  return [rc.paths, rc.ignored];
+}
+
+export const runParallelLint = (options) => {
+  const {
+    workers,
+    rcPath,
+  } = options;
+
+  const lintRunner = new LintRunner(workers);
+  const [paths, ignored] = processEsprintrc(rcPath);
+
+  let filePaths = [];
+  process.stdout.write("Collecting files...[this may take a little bit]");
+  for (let i = 0; i < paths.length; i++) {
+    const files = glob.sync(paths[i], {});
+    files.forEach((file, idx) => {
+      filePaths.push(file);
+    })
+  }
+
+  let filesToProcess = 0;
+  let lintResults = [];
+
+  filePaths.map((file, _) => {
+    if (eslint.isPathIgnored(path.join(ROOT_DIR, file)) || file.indexOf('eslint') !== -1) {
+      return;
+    }
+    filesToProcess++;
+    lintRunner.run({ cwd: ROOT_DIR }, [file])
+      .then((results) => {
+        const result = results[0];
+        lintResults.push(result);
+        filesToProcess--;
+      });
+  });
+
+  const interval = setInterval(() => {
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write(`Linting files...${filesToProcess} left to lint`);
+
+    if (filesToProcess === 0) {
+      clearInterval(interval);
+      lintResults = lintResults.filter((result) => {
+        return result.warningCount > 0 || result.errorCount > 0;
+      });
+      const formatter = eslint.getFormatter();
+      console.log(formatter(lintResults));
+      process.exit(0);
+    }
+  }, 1000);
+};
