c91c9b1bb996f73c6afc545b654cb04983e40e02
Allen Kleiner <allenk@pinterest.com>
2017-05-24 14:58:22 -0700

    Update install script to handle file-linked eslint dependencies

--- scripts/installDependencies.js
+++ scripts/installDependencies.js
@@ -4,21 +4,29 @@ const execSync = require('child_process').execSync;
 const readFileSync = require('fs').readFileSync;
 const path = require('path');
 
-const packageJson = JSON.parse(readFileSync(path.join(process.argv[2], 'package.json')));
+const dirPath = process.argv[2];
+const packageJson = JSON.parse(readFileSync(path.join(dirPath, 'package.json')));
 
 if (!packageJson) {
   console.log('No package.json found at the path specified. Exiting...');
   process.exit(0);
 }
 
-let command = 'npm i ';
+const install = 'npm i ';
+const link = 'npm ln '
+let dependencies = '';
 
 const devDeps = packageJson['devDependencies'];
 
 for (const key in devDeps) {
   if (key.indexOf('eslint') !== -1) {
-    command += `${key}@` + devDeps[key] + ' ';
+    // Manually link file: dependencies
+    if (devDeps[key].startsWith('file:')) {
+      execSync(`${link} ${path.join(dirPath, devDeps[key])}`);
+    } else {
+      dependencies += `${key}@${devDeps[key]} `;
+    }
   }
 }
 
-execSync(command);
+execSync(`${install} ${dependencies}`);
