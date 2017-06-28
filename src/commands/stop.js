c161f41e753583dbc79e1e2d089b63d646721a4c
Allen Kleiner <akleiner24@gmail.com>
2017-06-27 17:17:25 -0700

    Better handling of port-killing logic (#42)
    * Better handling of port-killing logic
    
    * Fix lint

--- src/commands/stop.js
+++ src/commands/stop.js
@@ -1,7 +1,18 @@
+import { readFileSync } from 'fs';
 import { execSync } from 'child_process';
 
+import { findFile } from '../util.js';
+
 export const stop = () => {
-  const command = 'lsof -i TCP:5004 | grep LISTEN | awk \'{print $2}\' | xargs kill -9';
-  execSync(command);
-  console.log('Server running on port 5004 found and stopped');
+  const filePath = findFile('.esprintrc');
+  const rc = JSON.parse(readFileSync(filePath));
+
+  if (rc.port) {
+    const { port } = rc;
+    const command = `lsof -i TCP:${port} | grep LISTEN | awk '{print $2}' | xargs kill -9`;
+    execSync(command);
+    console.log(`Server running on port ${port} found and stopped`);
+  } else {
+    console.warn('No port specified in `.esprintrc` file');
+  }
 };
